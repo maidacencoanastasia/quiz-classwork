acddf2bec81083743bbe4bd70f12d35428a87fc8
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-08 09:36:36 +0300

    add in controller

--- src/main/java/com/example/quiz/controller/QuestionController.java
+++ src/main/java/com/example/quiz/controller/QuestionController.java
@@ -1,13 +1,17 @@
 package com.example.quiz.controller;
 
+import com.example.quiz.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class QuestionController {
+    @Autowired
+    private QuestionRepository questionRepository;
     @GetMapping("/questions")
     public String getQuestions(Model model) {
         return "questions";
