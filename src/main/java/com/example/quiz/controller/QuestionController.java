cb5a4e09b2c5a7ee0b43f12d524a7accb8cf2f4f
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-10 23:41:10 +0300

    add methods implementation

--- src/main/java/com/example/quiz/controller/QuestionController.java
+++ src/main/java/com/example/quiz/controller/QuestionController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @Controller
+@RequestMapping("questions")
 public class QuestionController {
     @Autowired
     private QuestionRepository questionRepository;
---
+++ src/main/java/com/example/quiz/model/TrueAnswers.java
@@ -0,0 +1,6 @@
+package com.example.quiz.model;
+
+public class TrueAnswers {
+    private Question question;
+    private  boolean correct;
+}
