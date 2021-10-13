227f05457cea7d4731d6c39ce63e6c9cf0bb80d4
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-13 21:30:11 +0300

    add to QuestionRepository

--- src/main/java/com/example/quiz/repository/QuestionRepository.java
+++ src/main/java/com/example/quiz/repository/QuestionRepository.java
@@ -4,7 +4,7 @@ import com.example.quiz.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-    Question findTop1ByOrderBOrderByIdAsc();
-    Question findTop1ByIdGreaterThanOOrderByIdAsc();
+    Question findTop1ByOrderByIdAsc();
+    Question findTop1ByIdGreaterThanOrderByIdAsc(int id);
 
 }
