faf3366c19d5320a57824aae9f0bd14b5f7c53e2
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-13 22:02:40 +0300

    add html templates

--- src/main/java/com/example/quiz/controller/UserAnswerForm.java
+++ src/main/java/com/example/quiz/controller/UserAnswerForm.java
@@ -2,9 +2,17 @@ package com.example.quiz.controller;
 
 import lombok.Data;
 
-@Data
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Data
+@Entity
+@Table(name = "user_answer")
 public class UserAnswerForm {
+    @Id
+    @GeneratedValue
     private int id;
     private int choice;
 }
